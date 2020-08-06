db214872554471948206dbe002924e5c5783fc8e
sandra_c <chavezsandra0498@gmail.com>
2020-08-05 19:20:03 -0700

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -7,39 +7,64 @@ engine = ts.init()
 
 substring = 'important'
 
+indicators = ['important', 'take screenshot', 'need to remember this']
+exits = ['no more', 'exit', 'done']
 
 def erispeaks(phrase):
     print(phrase)
     engine.say(phrase)
     engine.runAndWait()
-    
+
+
+def getScreenshot(input):
+    if substring in input:
+        ss = p.screenshot('screenshot.png')
+        timestamp = 'screenshot taken in ', t.strftime("%I:%M:%S")
+        erispeaks(timestamp)
+    else:
+        pass
 
 def getAudio():
     variable = SR.Recognizer()
 
     with SR.Microphone() as receiver:
-        audioInput = variable.record(receiver, duration=5)
+        print('speak now...')
+        audioInput = variable.listen(receiver, phrase_time_limit=5)
+        print('done')
 
         try:
             textOutput = variable.recognize_google(audioInput)
-            if substring in textOutput:
-                ss = p.screenshot('screenshot.png')
-                timestamp = 'screenshot taken in ', t.strftime("%I:%M:%S")
-                erispeaks(timestamp)
-            else:
-                erispeaks('Did not take the screenshot!')
-    
+            return textOutput
+
+
         except SR.UnknownValueError:
             # either did not speak into the mic/ could not understand the input
             print('Could not process the audio!')
+            return 0 
 
 def main():
     while 1:
+        voiceInput = getAudio()
+        if voiceInput == 0:
+            continue
+            
+        voiceInput = str(voiceInput).lower()
         
-        getAudio()
+        if any(word in voiceInput for word in exits):
+            erispeaks('Done taking screenshots')
+            break
+    
+        getScreenshot(voiceInput)    
         
-    
-    
-    
+        
+        
+        
+        
+
+        
+
+
+
+
 if __name__ == "__main__":
     main()
