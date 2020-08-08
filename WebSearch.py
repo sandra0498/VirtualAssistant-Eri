2d20a862da4dcb779bfaabbaff7251a107d02071
sandra_c <chavezsandra0498@gmail.com>
2020-08-08 14:28:29 -0700

    Update WebSearch.py

--- WebSearch.py
+++ WebSearch.py
@@ -1,8 +1,30 @@
 import webbrowser as web
 import speech_recognition as SR
+import pyttsx3 as ts
+
+engine = ts.init()
+
+class web:
+
+    def __init__(self, site, subject):
+        self.site = site
+        self.subject = subject
 
 
-variable = SR.Recognizer()
+    def getAudioForSite(self):
+        variable = SR.Recognizer()
+        while 1:
+            with SR.Microphone as receiver:
+                self.erispeaks('speak now...')
+                
+
+
+    def erispeaks(self, phrase):
+        print(phrase)
+        engine.say(phrase)
+        engine.runAndWait()
+
+
 
 
 url = 'https://www.youtube.com/'
