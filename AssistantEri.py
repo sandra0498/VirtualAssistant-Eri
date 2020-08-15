5dc685e61c31ea605a1f8fcd73d7f4d50ea7a139
sandra_c <chavezsandra0498@gmail.com>
2020-08-15 14:37:22 -0700

    Update AssistantEri.py

--- AssistantEri.py
+++ AssistantEri.py
@@ -1,11 +1,15 @@
 import Translate as t  # importing the translation feature
 import screenshot as sc
-import WebSearch as ws
+import WebSearch as WS
 import speech_recognition as SR
 import pyttsx3 as ts
 
 
 engine = ts.init()
+
+# default voice
+voice_ID = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+engine.setProperty('voice', voice_ID)
 
 def eriSpeaks(phrase):
     print(phrase)
@@ -27,7 +31,11 @@ def getVoice():
             return 0
 
 def main():
+    eriSpeaks('Hello my name is Erendira but for short call me Eri')
+    eriSpeaks('what would you like me to do?')
     t.main()
+
+    WS.main()
 
 
 if __name__ == '__main__':
