644f0944b726f52cb56878f22b06ec1d44cc8767
sandra_c <chavezsandra0498@gmail.com>
2020-08-15 14:27:29 -0700

    Update AssistantEri.py

--- AssistantEri.py
+++ AssistantEri.py
@@ -1,8 +1,30 @@
 import Translate as t  # importing the translation feature
 import screenshot as sc
 import WebSearch as ws
+import speech_recognition as SR
+import pyttsx3 as ts
 
 
+engine = ts.init()
+
+def eriSpeaks(phrase):
+    print(phrase)
+    engine.say(phrase)
+    engine.runAndWait()
+
+def getVoice():
+    variable = SR.Recognizer()
+    with SR.Microphone() as source:
+        print('speak now...')
+        voice = variable.listen(source, phrase_time_limit=5)
+        print('done')
+        try:
+            text = variable.recognize_google(voice)
+            return text
+
+        except SR.UnknownValueError:
+            eriSpeaks("i'm sorry, couldn't get that")
+            return 0
 
 def main():
     t.main()
