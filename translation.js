0a1837fe43124700538fb9158df62ec0bb862e8b
sandra_c <chavezsandra0498@gmail.com>
2020-08-20 19:01:10 -0700

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,20 +1,27 @@
 var unirest = require("unirest");
 
-var req = unirest("GET", "https://microsoft-azure-translation-v1.p.rapidapi.com/translate");
+var req = unirest("POST", "https://microsoft-translator-text.p.rapidapi.com/translate");
 
 req.query({
-	"from": "en",
-	"to": "es",
-	"text": "Hello%2C world!"
+	"profanityAction": "NoAction",
+	"textType": "plain",
+	"api-version": "3.0"
 });
 
 req.headers({
-	"x-rapidapi-host": "microsoft-azure-translation-v1.p.rapidapi.com",
+	"x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
 	"x-rapidapi-key": "afa3404f83mshf2743be05f8bfeap15b74ejsn3b8517cde47b",
+	"content-type": "application/json",
 	"accept": "application/json",
 	"useQueryString": true
 });
 
+req.type("json");
+req.send([
+	{
+		"Text": "I would really like to drive your car around the block a few times."
+	}
+]);
 
 req.end(function (res) {
 	if (res.error) throw new Error(res.error);
