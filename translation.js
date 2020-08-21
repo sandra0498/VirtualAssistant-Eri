c1ea4baa0d296cb9d4a649e636adb179f8eff349
sandra_c <chavezsandra0498@gmail.com>
2020-08-20 19:05:19 -0700

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,30 +1,25 @@
-var unirest = require("unirest");
+const axios = require("axios");
 
-var req = unirest("POST", "https://microsoft-translator-text.p.rapidapi.com/translate");
-
-req.query({
-	"profanityAction": "NoAction",
-	"textType": "plain",
-	"api-version": "3.0"
-});
-
-req.headers({
-	"x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
-	"x-rapidapi-key": "afa3404f83mshf2743be05f8bfeap15b74ejsn3b8517cde47b",
-	"content-type": "application/json",
-	"accept": "application/json",
-	"useQueryString": true
-});
-
-req.type("json");
-req.send([
-	{
-		"Text": "I would really like to drive your car around the block a few times."
-	}
-]);
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+axios({
+    "method":"POST",
+    "url":"https://microsoft-translator-text.p.rapidapi.com/translate",
+    "headers":{
+    "content-type":"application/json",
+    "x-rapidapi-host":"microsoft-translator-text.p.rapidapi.com",
+    "x-rapidapi-key":"afa3404f83mshf2743be05f8bfeap15b74ejsn3b8517cde47b",
+    "accept":"application/json",
+    "useQueryString":true
+    },"params":{
+    "profanityAction":"NoAction",
+    "textType":"plain",
+    "api-version":"3.0"
+    },"data":[{
+    "Text":"I would really like to drive your car around the block a few times."
+    }]
+    })
+    .then((response)=>{
+      console.log(response)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
