77acd68c8c343732c643d90d91f508ee5dc4787d
sandra_c <chavezsandra0498@gmail.com>
2021-01-09 02:11:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ request.open("GET", "https://google-translate20.p.rapidapi.com/languages");
 request.setRequestHeader("x-rapidapi-key", "afa3404f83mshf2743be05f8bfeap15b74ejsn3b8517cde47b");
 request.setRequestHeader("x-rapidapi-host", "google-translate20.p.rapidapi.com");
 
-request.onload() = function() {
+request.onload = function() {
 
     var response = request.response;
     var parsedData = JSON.parse(response);
