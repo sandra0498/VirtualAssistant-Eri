4973b4d9f7207288ad9742e6695903587ff1025b
Sandra Chavez <chavezsandra0498@gmail.com>
2022-05-31 14:29:33 -0700

    Update userInterface.py

--- userInterface.py
+++ userInterface.py
@@ -1,10 +1,15 @@
 import dearpygui.dearpygui as dpg
+from dearpygui.core import *
+import pyttsx3 as ts
+engine = ts.init()
 
 
 
-def save_callback():
+def save_callback(name):
+    engine.say(name)
+    engine.runAndWait()
 
-    print("Save Clicked")
+    
 
 
 
@@ -16,11 +21,16 @@ dpg.setup_dearpygui()
 
 
 
-with dpg.window(label="Example Window"):
+with dpg.window(label="Assistant Eri"):
+
+
+    dpg.add_input_text(hint="Enter your name here", name="user")
+
+    dpg.add_button(label="Say hello")
 
     dpg.add_text("Hello world")
 
-    dpg.add_button(label="Save", callback=save_callback)
+    dpg.add_button(label="Save", callback=save_callback(user))
 
     dpg.add_input_text(label="string")
 
